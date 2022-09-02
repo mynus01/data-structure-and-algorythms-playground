c0ae8cfe28abc723b7f2036d0711abe2562910f1
Matheus Sandri <matheusgsandri@gmail.com>
2022-09-02 16:21:41 -0300

    refactor: refactor array tests

--- src/test/kotlin/CustomArrayTest.kt
+++ src/test/kotlin/CustomArrayTest.kt
@@ -1,14 +1,20 @@
 import array.MyCustomArray
+import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class CustomArrayTest {
-    @Test
-    fun `adding one more item than initial capacity should increase array size by one`() {
-        val array = MyCustomArray(3)
+    private val array = MyCustomArray(3)
+
+    @Before
+    fun setup() {
         array.add(0)
         array.add(100)
         array.add(200)
+    }
+
+    @Test
+    fun `adding one more item than initial capacity should increase array size by one`() {
         array.add(300)
 
         assertEquals(4, array.size())
@@ -16,40 +22,21 @@ class CustomArrayTest {
 
     @Test
     fun `getting a valid position should return its value`() {
-        val array = MyCustomArray(3)
-        array.add(0)
-        array.add(100)
-        array.add(200)
-
         assertEquals(200, array.get(2))
     }
 
     @Test(expected = IllegalArgumentException::class)
     fun `getting a position that doesn't exist should throw IllegalArgumentException`() {
-        val array = MyCustomArray(3)
-        array.add(0)
-        array.add(100)
-        array.add(200)
-
         array.get(3)
     }
 
     @Test(expected = IllegalArgumentException::class)
     fun `getting a negative position should throw IllegalArgumentException`() {
-        val array = MyCustomArray(3)
-        array.add(0)
-        array.add(100)
-        array.add(200)
-
         array.get(3)
     }
 
     @Test
     fun `removing a valid position should decrease array size by one`() {
-        val array = MyCustomArray(3)
-        array.add(0)
-        array.add(100)
-        array.add(200)
         array.removeAt(2)
 
         assertEquals(2, array.size())
@@ -57,29 +44,16 @@ class CustomArrayTest {
 
     @Test(expected = IllegalArgumentException::class)
     fun `removing a position that doesn't exist should throw IllegalArgumentException`() {
-        val array = MyCustomArray(3)
-        array.add(0)
-        array.add(100)
-        array.add(200)
         array.removeAt(3)
     }
 
     @Test(expected = IllegalArgumentException::class)
     fun `removing a negative number should throw IllegalArgumentException`() {
-        val array = MyCustomArray(3)
-        array.add(0)
-        array.add(100)
-        array.add(200)
         array.removeAt(-1)
     }
 
     @Test
     fun `if in array, indexOf should return position of the number`() {
-        val array = MyCustomArray(3)
-        array.add(0)
-        array.add(100)
-        array.add(200)
-
         val result = array.indexOf(200)
 
         assertEquals(2, result)
@@ -87,11 +61,6 @@ class CustomArrayTest {
 
     @Test
     fun `if not in array, indexOf should return -1`() {
-        val array = MyCustomArray(3)
-        array.add(0)
-        array.add(100)
-        array.add(200)
-
         val result = array.indexOf(300)
 
         assertEquals(-1, result)
